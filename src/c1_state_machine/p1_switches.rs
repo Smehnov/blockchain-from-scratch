4ef08465e6f668d35ce985b2d78a17d19a0d20d3
alex <alexander.smehnov@gmail.com>
2024-05-27 12:18:16 +0800

    c1-p1 done

--- src/c1_state_machine/p1_switches.rs
+++ src/c1_state_machine/p1_switches.rs
@@ -15,7 +15,7 @@ impl StateMachine for LightSwitch {
     type Transition = ();
 
     fn next_state(starting_state: &bool, t: &()) -> bool {
-        todo!("Exercise 1")
+        return !starting_state
     }
 }
 
@@ -42,7 +42,24 @@ impl StateMachine for WeirdSwitchMachine {
     type Transition = Toggle;
 
     fn next_state(starting_state: &TwoSwitches, t: &Toggle) -> TwoSwitches {
-        todo!("Exercise 2")
+        let mut res_state = TwoSwitches{
+            first_switch: starting_state.first_switch,
+            second_switch: starting_state.second_switch
+        };
+        match t{
+            Toggle::FirstSwitch=>{
+                if starting_state.first_switch{
+                    res_state.second_switch = false;
+                }
+                res_state.first_switch = !res_state.first_switch;
+                return res_state;
+            },
+
+            Toggle::SecondSwitch=>{
+               res_state.second_switch = !starting_state.first_switch; 
+                return res_state;
+            }
+        }
     }
 }
 
